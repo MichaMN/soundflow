beae69b44ba13aba5b1cd3cdcfd80b4b2355b305
Micha <michamuller.n@gmail.com>
2022-07-17 20:43:06 +0200

    Update Sound Component

--- src/components/sound/Sound.jsx
+++ src/components/sound/Sound.jsx
@@ -1,12 +1,16 @@
-import React, { createRef } from "react";
-import AudioPlayer from "react-h5-audio-player";
-import "react-h5-audio-player/lib/styles.css";
+import React, { useState } from "react";
+import ReactHowler from "react-howler";
 
 const Sound = (props) => {
   const { name, playCount, setPlayCount } = props;
+  const [volume, setVolume] = useState(0.5);
+  const [play, setPlay] = useState(false);
   const audio = require("../../assets/audio/" + name + ".wav");
-  const iconNode = <i className={"icon-" + name}></i>;
-  const ref = createRef();
+
+  const handlePlay = () => {
+    setPlay(!play);
+    play ? addPlayCount() : subtractPlayCount();
+  };
 
   const addPlayCount = () => {
     setPlayCount(playCount + 1);
@@ -16,22 +20,14 @@ const Sound = (props) => {
   };
 
   return (
-    <div className="audio__wrapper">
-      <AudioPlayer
-        src={audio}
-        onPlay={addPlayCount}
-        onPause={subtractPlayCount}
-        loop
-        customIcons={{ play: iconNode, pause: iconNode }}
-        showJumpControls={false}
-        showDownloadProgress={false}
-        showFilledVolume={true}
-        className="player"
-        layout="horizontal-reverse"
-        customProgressBarSection={[]}
-        customControlsSection={["MAIN_CONTROLS", "VOLUME_CONTROLS"]}
-        ref={ref}
-      />
+    <div className="audio__wrapper" onClick={handlePlay}>
+      <ReactHowler src={audio} playing={play} volume={volume} loop={true} />
+
+      <i className={"icon-" + name}></i>
+
+      <div className="audio__volume">
+        <input type="range" min="0" max="1" step=".05" value={volume} onChange={(e) => setVolume(parseFloat(e.target.value))} />
+      </div>
     </div>
   );
 };
